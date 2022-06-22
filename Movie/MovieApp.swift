15133459c2600b0d8a57166783df60b61f5ae760
Diosenelth <delahozdiosenelth216@gmail.com>
2022-06-22 18:07:25 -0500

    Initial Commit

---
+++ Movie/MovieApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieApp.swift
+//  Movie
+//
+//  Created by Diosenelth De la hoz Fonseca on 22/06/22.
+//
+
+import SwiftUI
+
+@main
+struct MovieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
